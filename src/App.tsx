637aa1226ec9cdaf7d80d8757db3a54c4dcc76b7
MWayneMercado <MWayneMercado@gmail.com>
2022-03-28 12:50:54 -0700

    define route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { PrivateRoute, ReversePrivateRoute } from './Router/PrivateRoutes';
+
+import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path = '/' element={ <ReversePrivateRoute><Login/></ReversePrivateRoute> }/>
+          <Route path = '/home' element={ <PrivateRoute><Home/></PrivateRoute> }/>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
