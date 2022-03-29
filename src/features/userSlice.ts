d5e93f060141e79417a8b1aa012237449ca43faa
MWayneMercado <MWayneMercado@gmail.com>
2022-03-29 16:20:06 -0700

    implement user state in redux store

---
+++ src/features/userSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// interface UserState {
+//   user: {
+//     _id: string;
+//     firstname: string;
+//     lastname: string;
+//     username: string;
+//     about: string;
+//     pfpURL: string;
+//     posts: string[];
+//     friends: string[];
+//     requests: string[];
+//     requested: string[];
+//   }
+// }
+
+const initialState = {
+  // user: {
+  //   _id: '',
+  //   firstname: '',
+  //   lastname: '',
+  //   username: '',
+  //   about: '',
+  //   pfpURL: '',
+  //   posts: [],
+  //   friends: [],
+  //   requests: [],
+  //   requested: [],
+  // }
+  user: {}
+}
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    addUser: (state, action: PayloadAction<object>) => {
+      state.user = action.payload;
+    }
+  },
+})
+
+export default userSlice.reducer;
