f0f35c209455e7579eb758bc4a06bef5258dc2f1
MWayneMercado <MWayneMercado@gmail.com>
2022-04-05 13:52:29 -0700

    use nav state to toggle display

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,25 +1,32 @@
 import './Header.scss';
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../app/store';
+import { setNav } from '../features/navSlice';
 
 import {ReactComponent as Profile} from '../icons/person_black_24dp.svg';
 
 import Nav from '../Nav';
 
 function Header() {
-  const [navToggled, setNavToggled] = useState(false);
+  const nav: boolean = useSelector((state: RootState) => state.nav.value);
+  const dispatch = useDispatch();
+
+  const handleNavToggle = () => {
+    dispatch(setNav(!nav));
+  };
 
   return (
     <>
       <div className="Header">
         <a href="/#/home" className="headerTextLogo">Odinbook</a>
         <div className="headerUtils">
-          <button className="headerNavToggle" onClick={() => {setNavToggled(!navToggled)}}>
+          <button className="headerNavToggle" onClick={() => {handleNavToggle()}}>
             <div className="profIconWrapper"><Profile className="profIcon"/></div>
           </button>
         </div>
       </div>
       {
-        navToggled ?
+        nav ?
         <Nav />
         :
         null
