c98e175112fec11300a0f84a38a8aabe067189c4
MWayneMercado <MWayneMercado@gmail.com>
2022-04-04 18:08:20 -0700

    re-implement post comments

--- src/FeedItem/FeedItem.tsx
+++ src/FeedItem/FeedItem.tsx
@@ -6,7 +6,7 @@ import {ReactComponent as ThumbUp} from '../icons/thumb_up_black_24dp.svg';
 import { Post } from '../interfaces/Post';
 import { User } from '../interfaces/User';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { ReactEventHandler, useEffect, useState } from 'react';
 import { RootState } from '../app/store';
 
 const he = require('he');
@@ -18,6 +18,12 @@ function FeedItem(props: any) {
   const { userid, post } = props;
 
   const [likeClass, setLikeClass] = useState("");
+
+  const [showComments, setShowComments] = useState(false);
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState(Array(post.comments.length));
+  const [numComments, setNumComments] = useState(post.comments.length);
+
 
   const handleLike = async () => {
     if (!post.likes.includes(userid)) {
@@ -60,6 +66,68 @@ function FeedItem(props: any) {
     }
   };
 
+  const togComments = () => {
+    getComments();
+    setShowComments(!showComments);
+  };
+
+  const createComment = async () => {    
+    let urlParams = new URLSearchParams();
+    urlParams.append('text', commentText);
+
+    try {
+      let response = await fetch(`/users/${userid}/posts/${post._id}/comments`, {
+        method: "POST",
+        body: urlParams,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        credentials: "include",
+      });
+
+      const parsedResponse = await response.json();
+      
+      if (response.status === 201) {
+        console.log("Comment saved");
+        getComments();
+      } else {
+        console.log("Error on comment POST")
+        console.log(parsedResponse.err.errors);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const getComments = async () => {
+    try {
+      let response = await fetch(`/users/${userid}/posts/${post._id}/comments`, {
+        method: "GET",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        credentials: "include",
+      });
+      
+      const parsedResponse = await response.json();
+
+      if (response.status === 200) {
+        setComments([]);
+        parsedResponse.forEach((commentObj: object) => {
+          setComments((comments) => [...comments, commentObj]
+          .sort(function(a,b){
+            return new Date(b.timestamp).valueOf() - new Date(a.timestamp).valueOf();
+          }));
+        });
+      } else {
+        console.log("Error on comments list GET")
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmitComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createComment();
+  }
+
   useEffect(() => {
     let calcClass = post.likes.includes(userid) ? "ThumbUp__container ThumbUp__container--liked" : "ThumbUp__container";
     setLikeClass(calcClass);
@@ -90,41 +158,22 @@ function FeedItem(props: any) {
           }
           <div className="FeedItem__author">{post.authorString}</div>
         </span>
-        {/* <span className="flexWrapper">
-          {
-            currentUserIsTarget ?
-            <button className="EditItem__editBtn" onClick={deletePost}>Delete</button>
-            :
-            null
-          }
-          {
-            currentUserIsTarget ?
-            <span>
-            {
-              !editing ?
-              <button className="EditItem__editBtn" onClick={toggleEdit}>Edit</button>
-              :
-              <button className="EditItem__editBtn" onClick={updatePost}>Done Editing</button>
-            }
-            </span>
-            :
-            null
-          }
-          <div className="FeedItem__timestamp">
-            {new Date(props.post.timestamp).toLocaleDateString("en-US",
-            { hour: '2-digit',
-              hour12: 'true',
-              minute: '2-digit',
-            })}
-            </div>
-        </span> */}
+        <div className="FeedItem__timestamp">
+          {new Date(props.post.timestamp).toLocaleDateString("en-US",
+          { hour: '2-digit',
+            hour12: true,
+            minute: '2-digit',
+          })}
+          </div>
       </div>
+
       <span>
         <div className="FeedItem__title">{he.decode(post.title)}</div>
         <div className="FeedItem__text">
           {he.decode(post.text)}
         </div>
       </span>
+
       {
         post.imgURL ?
         <div className="FeedItem__imgContainer">
@@ -135,6 +184,7 @@ function FeedItem(props: any) {
         :
         null
       }
+
       <div className="FeedItem__stats">
         <div className="FeedItem__likes">
           <button onClick={handleLike} className={likeClass}><ThumbUp className="ThumbUp__svg"/></button>
@@ -146,6 +196,57 @@ function FeedItem(props: any) {
           }
         </div>
         <div className="FeedItem__isEdited"></div>
+      </div>
+
+      <div className="FeedItem__commentList">
+        <form className="commentForm" onSubmit={handleSubmitComment}>
+          <input className="commentForm__input" type="text" placeholder="New Comment"
+            value={commentText}
+            onChange={e => setCommentText(e.target.value)}
+            maxLength={100}
+            minLength={1}
+          />
+          <input className="commentForm__submit" type="submit" value="Publish"/>
+        </form>
+        <button className="toggleComments" onClick={togComments}>
+          {
+            showComments ?
+            "Hide comments"
+            :
+            <span>
+            {
+              numComments > 0 ?
+              <span>
+              {`Show ${numComments} comments`}
+              </span>
+              :
+              'Comments'
+            }
+            </span>
+            
+          }
+        </button>
+        {
+          showComments ? 
+          <div className={comments.length > 0 ? "commentList" : ''}>
+            {
+                comments.length > 0 ?
+                comments.map((item, i) => 
+                  <div className="comment" key={i}>
+                    <div className="sampleComment">
+                      {/* <div className="FeedItem__profIcon"><Profile className="FeedItem__icon"/></div> */}
+                      <div className="sampleComment__author">{item.authorString}:</div>
+                      <div className="sampleComment__text">{he.decode(item.text)}</div>
+                    </div>
+                  </div>
+                )
+                :
+                <div className="FeedItem__text">No comments exist. Be the first one to comment!</div>
+              }
+            </div>
+          :
+          null
+        }
       </div>
     </div>
   )
