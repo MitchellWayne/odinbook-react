a8dcd3ca9edae99c43c275ffc7e133a18d68c8a1
MWayneMercado <MWayneMercado@gmail.com>
2022-04-06 15:18:15 -0700

    refactor main nav into component

---
+++ src/Nav/Main/Main.tsx
@@ -0,0 +1,65 @@
+import './Main.scss';
+import { Link, useNavigate } from 'react-router-dom';
+
+import {ReactComponent as Profile} from '../../icons/person_black_24dp.svg';
+
+function Main(props: any) {
+  const { user } = props;
+  const navigate = useNavigate();
+
+  const logout = async () => {  
+    try {
+      let response = await fetch("/users/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+  
+      if (response.status !== 200) {
+        console.log("Logout error. See API console for more details");
+      }
+      document.cookie = 'loggedIn=; expires=0';
+      document.cookie = 'userId=; expires=0';
+      navigate('/');
+    } catch (err){
+      // API fetch fail
+      console.log("Error on User Logout POST: \n" + err);
+    }
+  }
+
+  return (
+    <div className="Main">
+      <span>
+        {
+          user.pfpURL ?
+          <img className="profPicContainer" alt="user pfp"
+            src={`/users/${user._id}/pfpS3/${user.pfpURL}`}>
+          </img>
+          :
+          <div className="profPicContainer"><Profile className="profPic"/></div>
+        }
+        <ul className="optionList">
+          <li>
+            <Link
+              className="option"
+              to="/profile">
+              My Profile
+            </Link>
+          </li>
+          <li><button className="option" >Friends</button></li>
+          {/* <li><a className="option" href="/#/post">Create Post</a></li> */}
+          <li>
+            <Link
+              className="option"
+              to="/post">
+              Create Post
+            </Link>
+          </li>
+          <li><a className="option" href="/#/users">Find Users</a></li>
+          <li><button className="option" onClick={logout}>Log Out</button></li>
+        </ul>
+      </span>
+    </div>
+  )
+}
+
+export default Main;
--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,70 +1,18 @@
 import './Nav.scss'
-
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 
+import Main from './Main';
 import { User } from '../interfaces/User';
-
-import {ReactComponent as Profile} from '../icons/person_black_24dp.svg';
 
 function Nav() {
   const user: User = useSelector((state: RootState) => state.user.value as User);
-
-
-  const navigate = useNavigate();
-
-  const logout = async () => {  
-    try {
-      let response = await fetch("/users/logout", {
-        method: "POST",
-        credentials: "include",
-      });
-  
-      if (response.status !== 200) {
-        console.log("Logout error. See API console for more details");
-      }
-      document.cookie = 'loggedIn=; expires=0';
-      document.cookie = 'userId=; expires=0';
-      navigate('/');
-    } catch (err){
-      // API fetch fail
-      console.log("Error on User Logout POST: \n" + err);
-    }
-  }
+  const [uiSelector, setUiSelector] = useState('default');
 
   return (
     <div className="Nav">
-      <span>
-        {
-          user.pfpURL ?
-          <img className="profPicContainer" alt="user pfp"
-            src={`/users/${user._id}/pfpS3/${user.pfpURL}`}>
-          </img>
-          :
-          <div className="profPicContainer"><Profile className="profPic"/></div>
-        }
-        <ul className="optionList">
-          <li>
-            <Link
-              className="option"
-              to="/profile">
-              My Profile
-            </Link>
-          </li>
-          <li><button className="option" >Friends</button></li>
-          {/* <li><a className="option" href="/#/post">Create Post</a></li> */}
-          <li>
-            <Link
-              className="option"
-              to="/post">
-              Create Post
-            </Link>
-          </li>
-          <li><a className="option" href="/#/users">Find Users</a></li>
-          <li><button className="option" onClick={logout}>Log Out</button></li>
-        </ul>
-      </span>
+      <Main user={user}/>
     </div>
   )
 }
